2fcf395826656ac49c924781cf5a3f56c9269d67
Lav Yadav <yadavlav571@gmail.com>
2024-05-03 21:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+{
+	  if (arr.length <= 1) return 0;
+
+    let cost = 0;
+
+    // Sort the array in non-decreasing order
+    arr.sort((a, b) => a - b);
+
+    // Merge the ropes pairwise
+    for (let i = 0; i < arr.length - 1; i++) {
+        let sum = arr[i] + arr[i + 1];
+        cost += sum;
+        // Replace the two ropes with their merged length
+        arr[i + 1] = sum;
+    }
+
+    return cost;
+	
+
 }
 
 module.exports=mincost;
